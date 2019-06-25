e0be7b8f4038522b7ab8daf7707d02d03bf8e5e6
Carl <pcs88pc@gmail.com>
2019-06-25 21:24:23 +0900

    02-5 튜플 / 딕셔너리 데이터 공부

--- 2장/list_related_func.py
+++ 2장/list_related_func.py
@@ -39,6 +39,28 @@ a.insert(2,5)
 print(a)
 
 #리스트 요소 제거(remove)
-a = [1,2,3,4,5,6]
+a = [1,2,3,4,5,6,3]
 a.remove(3)
 print(a)
+a.remove(3)
+print(a)
+
+#리스트 요소 끄집어내기(pop)
+a = [1,2,3]
+a.pop() #맨 마지막 요소를 돌려주고 그 요소는 삭제하는 함수이다.
+print(a)
+
+a = ['1','2','3']
+a.pop(1) #pop(x)는 x번째 요소를 돌려 주고 그 요소는 삭제한다.
+print(a)
+
+#리스트에 포함된 요소 x의 개수 세기(count)
+a = [1,2,3,1]
+print(a.count(1))
+
+#리스트 확장(extend) / extend(x)에서 x는 리스트만 올 수 있으며 원래의 a리스트에 x리스트를 더하게 된다.
+a = [1,2,3]
+a.extend([4,5])
+print(a)
+
+
